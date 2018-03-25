691c62178296f1dab51425d2a27dfc76f7b820c5
iambambi <bacsabi5@gmail.com>
2018-03-25 15:38:24 +0100

    Created the base of project and added the .gitignore
    I created the solution and maked the layers as projects. At the build I followed the official recommendations.
    
    The layers of solution:
    - The EntityFramework Core database context classes and entity repositories will go to the CryptoTrader.EntityFramework project along with a generic CRUD (Create-Read-Update-Delete) repository
    - The collection of your entity classes that you wish to store in the database will go to the CryptoTrader.EntityDomain project
    - The DTO (Data Transfer Object) classes -which are those versions of the database entities that you send to the client- will be in the CryptoTrader.DTO project
    - The CryptoTrader.Services project will contain all application services that connects the Controllers and the database
    - The CryptoTrader.Tests is a (x)Unit test project. Later this project will contain the unit tests

---
+++ CryptoTrader/CryptoTrader/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CryptoTrader.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
